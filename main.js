91c2f91804daeca8a3036dcbb0a0d9b256dc5d1c
Julian Toscani <julian_toscani@gmx.de>
2021-12-20 23:25:32 +0100

    update main.js for dev mode

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
+const isDev = process.env.IS_DEV == "true" ? true : false;
 
 function createWindow() {
   const win = new BrowserWindow({
@@ -7,10 +8,19 @@ function createWindow() {
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
     },
   });
 
-  win.loadFile("dist/index.html");
+  win.loadURL(
+    isDev
+      ? "http://localhost:3000"
+      : `file://${path.join(__dirname, "../dist/index.html")}`
+  );
+
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
 }
 
 app.whenReady().then(() => {
