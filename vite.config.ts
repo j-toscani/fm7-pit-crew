611272a255ea6e810188c23b7fa822fecdb7ff3e
Julian Toscani <julian_toscani@gmx.de>
2021-12-20 23:26:33 +0100

    update vite config for build script

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: path.resolve(__dirname, "./dist/"),
+  base: process.env.ELECTRON == "true" ? "./" : "",
   plugins: [vue()],
 });
