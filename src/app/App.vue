4146fe7c822596920bfa95c13f3daaced07d56d6
Julian Toscani <julian_toscani@gmx.de>
2021-12-21 23:49:35 +0100

    update app

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,12 +1,16 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { defineComponent } from "@vue/runtime-core";
+
+export default defineComponent({
+  mounted() {},
+});
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div>
+    <h1>Hello World!</h1>
+  </div>
 </template>
 
 <style>
